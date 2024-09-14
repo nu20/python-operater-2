2c0a20b21b6e5163860ac3847a5f2939f7aeb4f5
nu20 <bibhutipatra123@gmail.com>
2024-09-14 18:44:36 +0530

    first commit

---
+++ a2.py
@@ -0,0 +1,10 @@
+marks = int(input("enter your marks"))
+if (marks > 90):
+    print ("you will get a new laptop")
+
+elif (marks>80 and marks<90):
+    print("You will get a ipad")
+elif (marks >70 and marks<80):
+    print("you will get a phone")
+else :
+    print("you will get an nothing72")
---
+++ main.py
@@ -0,0 +1,5 @@
+marks = int(input("enter your marks"))
+if (marks > 90):
+    print ("you will get a new phone")
+else :
+    print("you will not get anything")
